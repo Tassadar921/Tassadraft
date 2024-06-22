baba9e5e68bcc74f0ad6b768ca6420fc631ce52c
Tassadar921 <paul.lecuisinier@gmail.com>
2024-06-22 13:02:40 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // appApiBaseUrl: 'https://api.tassadraft.com',
-  appApiBaseUrl: 'http://localhost:3333',
+  appApiBaseUrl: 'https://api.tassadraft.com',
+  // appApiBaseUrl: 'http://localhost:3333',
   scryfallApiBaseUrl: 'https://api.scryfall.com',
   currenciesApiUrl: 'https://cdn.taux.live/api/ecb.json',
 };
